3551d9d6eeb168087b8237b4d582192ef29424fb
root <root@IMLTW065.impelsys.com>
2025-01-24 11:32:46 +0000

    some cleanups

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,12 +3,9 @@ import './poker.css'
 
 const Card = ({ value, onClick, index, enablePolling, isJira}) => {
   const unknown = "question mark"
-  // console.log(isDescription, isJira);
   return (
       
       <button id={value} disabled={(!enablePolling)} className="cardBox" onClick={onClick}>
-        
-       <label className="visuallyhidden" >Pointing Poker Point {value==="?" ? unknown : value }</label>
       <span  className="values">{value}</span>
     </button>
      
